3de0795637b27297bf2cb05f08d2d45a6857f50d
Administrator@RCNTV.LOC <Administrator@RCNTV.LOC>
2019-06-08 12:42:10 -0500

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -39,8 +39,8 @@ export class LoginComponent implements OnInit {
         });
         // this.user.email = "administrator";
         // this.user.password = "$S4ntiago12345"
-        this.user.email = "Jicastaneda";
-        this.user.password = "jorlinluchjuan-2019"
+        this.user.email = "";//"Jicastaneda";
+        this.user.password = "";//"jorlinluchjuan-2019"
     }
     private pushSettings = {
         // Android settings
