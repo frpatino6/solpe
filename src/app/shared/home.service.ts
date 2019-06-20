35ac009e3e15f6d825b9e10b32865617259289c2
frpatino6 <frpatino6@gmail.com>
2019-06-20 11:27:27 -0500

    se modifica hint pantalla login

--- src/app/shared/home.service.ts
+++ src/app/shared/home.service.ts
@@ -16,7 +16,7 @@ export class HomeService {
         
     }
 
-    private serverUrl = "http://solpe.rcntv.com.co:8082/solpe/GetLiberaSolpes/";
+    private serverUrl = "http://solpe.rcntv.com.co:8082/solpe/solpe/SendNotifications/";
     private serverDatabaseUrl="http://solpe.rcntv.com.co:8082/solpe/UpdateOrderState/"
 
     // private serverUrl = "http://192.168.0.6/solpeoracle/solpe/GetLiberaSolpes/";
