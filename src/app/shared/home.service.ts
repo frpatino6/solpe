6cd801b56401ecd60d2eaaf7e00a0486d2d4ad60
frpatino6 <frpatino6@gmail.com>
2019-06-25 11:53:28 -0500

    url puerto 444

--- src/app/shared/home.service.ts
+++ src/app/shared/home.service.ts
@@ -15,8 +15,8 @@ export class HomeService {
         
     }
 
-    private serverUrl = "https://solpe.rcntv.com.co/solpe/GetLiberaSolpes/";
-    private serverDatabaseUrl="https://solpe.rcntv.com.co/solpe/UpdateOrderState/"
+    private serverUrl = "https://solpe.rcntv.com.co:444/solpe/GetLiberaSolpes/";
+    private serverDatabaseUrl="https://solpe.rcntv.com.co:444/solpe/UpdateOrderState/"
     // private serverUrl = "http://192.168.0.5/solpeoracle/solpe/GetLiberaSolpes/";
     // private serverDatabaseUrl="http://192.168.0.5/solpeoracle/solpe/UpdateOrderState/"
 
